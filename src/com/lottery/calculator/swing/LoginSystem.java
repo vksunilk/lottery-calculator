2c9b1e986ce91967084fb83223de5acfd24fe77c
Sunil <Sunil@Sunil>
2019-04-19 13:50:19 +0530

    table model added

--- src/com/lottery/calculator/swing/LoginSystem.java
+++ src/com/lottery/calculator/swing/LoginSystem.java
@@ -1,22 +1,19 @@
 package com.lottery.calculator.swing;
 
-import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
 import javax.swing.JPasswordField;
 import javax.swing.JSeparator;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import com.lottery.calculator.biz.authenticate.AuthenticateLoginService;
-
-import java.awt.Font;
-import java.awt.SystemColor;
-import java.awt.Color;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class LoginSystem {
 
@@ -26,7 +23,6 @@ public class LoginSystem {
 	private JTextField txtUserId;
 	private JPasswordField passwordField;
 
-	
 	/**
 	 * Create the application.
 	 */
@@ -40,10 +36,10 @@ public class LoginSystem {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 335);
+		frame.setBounds(100, 100, 423, 290);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JLabel lblTitle1 = new JLabel("WELCOME TO LOTTERY CALCULATOR");
 		lblTitle1.setForeground(SystemColor.textHighlight);
 		lblTitle1.setBackground(SystemColor.text);
@@ -52,45 +48,45 @@ public class LoginSystem {
 		lblTitle1.setToolTipText("");
 		lblTitle1.setBounds(84, 24, 247, 14);
 		frame.getContentPane().add(lblTitle1);
-		
+
 		JLabel lblTitle2 = new JLabel("Login");
 		lblTitle2.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblTitle2.setForeground(SystemColor.textHighlight);
 		lblTitle2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitle2.setBounds(165, 47, 65, 14);
 		frame.getContentPane().add(lblTitle2);
-		
+
 		JLabel lblUserId = new JLabel("User Id");
 		lblUserId.setBounds(49, 90, 46, 14);
 		frame.getContentPane().add(lblUserId);
-		
+
 		JLabel lblPassword = new JLabel("Password");
 		lblPassword.setBounds(49, 137, 59, 14);
 		frame.getContentPane().add(lblPassword);
-		
+
 		txtUserId = new JTextField();
 		txtUserId.setToolTipText("Enter your user id here");
 		txtUserId.setBounds(154, 87, 166, 20);
 		frame.getContentPane().add(txtUserId);
 		txtUserId.setColumns(10);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setToolTipText("Enter your password here");
 		passwordField.setBounds(154, 134, 166, 20);
 		frame.getContentPane().add(passwordField);
-		
+
 		JButton btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if (authenticateLogin.authenticate(txtUserId.getText(), passwordField.getPassword())) {
-					transactionInfo= new TransactionInfo();
+					transactionInfo = new TransactionInfo();
 					transactionInfo.setVisible(true);
 				}
 			}
 		});
 		btnLogin.setBounds(50, 199, 78, 23);
 		frame.getContentPane().add(btnLogin);
-		
+
 		JButton btnReset = new JButton("Reset");
 		btnReset.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -100,7 +96,7 @@ public class LoginSystem {
 		});
 		btnReset.setBounds(165, 199, 78, 23);
 		frame.getContentPane().add(btnReset);
-		
+
 		JButton btnExit = new JButton("Exit");
 		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -109,15 +105,15 @@ public class LoginSystem {
 		});
 		btnExit.setBounds(280, 199, 78, 23);
 		frame.getContentPane().add(btnExit);
-		
+
 		JSeparator separator = new JSeparator();
 		separator.setBounds(67, 174, 1, 2);
 		frame.getContentPane().add(separator);
-		
+
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setBounds(49, 177, 309, 11);
 		frame.getContentPane().add(separator_1);
-		
+
 		JSeparator separator_2 = new JSeparator();
 		separator_2.setBounds(49, 72, 309, 11);
 		frame.getContentPane().add(separator_2);
--- src/com/lottery/calculator/swing/TransactionInfo.java
+++ src/com/lottery/calculator/swing/TransactionInfo.java
@@ -1,18 +1,22 @@
 package com.lottery.calculator.swing;
 
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JSeparator;
-import java.awt.Color;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.Button;
+import java.awt.Color;
 import java.awt.Font;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
 import com.toedter.calendar.JDateChooser;
 
 public class TransactionInfo extends JFrame {
@@ -23,6 +27,7 @@ public class TransactionInfo extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField textField;
+	private JTable table;
 
 	/**
 	 * Create the frame.
@@ -37,12 +42,12 @@ public class TransactionInfo extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		Button button = new Button("Edit Transactions");
 		button.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
 		button.setBounds(30, 10, 130, 33);
 		contentPane.add(button);
-		
+
 		Button button_1 = new Button("Add Customers");
 		button_1.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
 		button_1.addActionListener(new ActionListener() {
@@ -51,22 +56,22 @@ public class TransactionInfo extends JFrame {
 		});
 		button_1.setBounds(302, 10, 130, 33);
 		contentPane.add(button_1);
-		
+
 		JSeparator separator = new JSeparator();
 		separator.setBounds(29, 49, 564, 2);
 		contentPane.add(separator);
-		
+
 		textField = new JTextField();
-		textField.setBounds(142, 105, 215, 20);
+		textField.setBounds(142, 104, 226, 21);
 		contentPane.add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblCustomer = new JLabel("Customer Name");
 		lblCustomer.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		lblCustomer.setLabelFor(textField);
-		lblCustomer.setBounds(30, 107, 94, 17);
+		lblCustomer.setBounds(30, 103, 94, 21);
 		contentPane.add(lblCustomer);
-		
+
 		JButton btnNewButton = new JButton("Search");
 		btnNewButton.setFont(new Font("Arial", Font.PLAIN, 11));
 		btnNewButton.addActionListener(new ActionListener() {
@@ -75,28 +80,45 @@ public class TransactionInfo extends JFrame {
 		});
 		btnNewButton.setBounds(390, 104, 104, 23);
 		contentPane.add(btnNewButton);
-		
+
 		JButton btnClear = new JButton("Clear");
 		btnClear.setFont(new Font("Arial", Font.PLAIN, 11));
 		btnClear.setBounds(504, 104, 89, 23);
 		contentPane.add(btnClear);
-		
+
 		Button button_2 = new Button("Print Transactions");
 		button_2.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
 		button_2.setBounds(166, 10, 130, 33);
 		contentPane.add(button_2);
-		
+
 		JLabel lblNewLabel = new JLabel("Transaction Date");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		lblNewLabel.setBounds(29, 62, 94, 20);
 		contentPane.add(lblNewLabel);
-		
-		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setBounds(142, 62, 91, 20);
+
+		final JDateChooser dateChooser = new JDateChooser();
+		dateChooser.setBounds(142, 62, 105, 21);
 		contentPane.add(dateChooser);
-		
-		JDateChooser dateChooser_1 = new JDateChooser();
-		dateChooser_1.setBounds(257, 62, 91, 20);
+
+		final JDateChooser dateChooser_1 = new JDateChooser();
+		dateChooser_1.setBounds(257, 62, 111, 21);
 		contentPane.add(dateChooser_1);
+
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(30, 134, 564, 2);
+		contentPane.add(separator_1);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(30, 147, 563, 265);
+		contentPane.add(scrollPane);
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText("");
+				dateChooser.setDate(new Date());
+				dateChooser_1.setDate(new Date());
+			}
+		});
+		table = new JTable();
+		scrollPane.setViewportView(table);
 	}
 }
